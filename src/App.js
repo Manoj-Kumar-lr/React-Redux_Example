792f0a4287e71b5a2e475d9013925f6413dd951f
Manoj Kumar L R <iam.manoj34lr@gmail.com>
2022-02-03 23:05:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+import React from "react";
+import { Main } from "./Components/Main";
+import { User } from "./Components/User";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Virat Kohli"
+    };
+  }
+
+  setName = (name) => {
+    console.log(name);
+    this.setState({
+      name: name
+    });
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <button onClick={() => this.setName("Virat")}> Change Name </button>
+        <h1>{this.state.name}</h1>
+        <Main />
+        <User />
+      </div>
+    );
+  }
 }
+
+export default App;
